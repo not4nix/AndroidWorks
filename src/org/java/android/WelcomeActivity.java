6170705d194f0fc3f92300e731bde08f4a035979
Viacheslav Okolitiy <okolitiy.viacheslav@gmail.com>
2012-01-28 01:18:27 +0200

    New layouts commited

--- src/org/java/android/WelcomeActivity.java
+++ src/org/java/android/WelcomeActivity.java
@@ -1,5 +1,14 @@
 package org.java.android;
 
-public class WelcomeActivity {
-	
+import android.app.Activity;
+import android.os.Bundle;
+
+public class WelcomeActivity extends Activity{
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.welcome_layout);
+	}
 }
