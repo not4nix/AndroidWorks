2846ad727a01a0e9fd7f5cb090198cd2fde0ed97
Viacheslav Okolitiy <okolitiy.viacheslav@gmail.com>
2012-01-30 15:16:25 +0200

    First menu completed

--- src/org/java/android/BuyersGuideActivity.java
+++ src/org/java/android/BuyersGuideActivity.java
@@ -1,5 +1,6 @@
 package org.java.android;
 
+import java.net.URL;
 import java.util.ArrayList;
 
 import org.java.android.adapter.JsonArrayAdapter;
@@ -8,22 +9,38 @@ import org.java.android.task.JsonTask;
 import org.java.android.R;
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.os.Bundle;
+import android.view.MenuItem.OnMenuItemClickListener;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import android.widget.ProgressBar;
+import android.widget.Toast;
 
-public class BuyersGuideActivity extends Activity {
+public class BuyersGuideActivity extends Activity{
 	private static final String id = "id";
+	
     private static final String name = "name";
+    
     private static final String url = "url";
+    
     private static final String icon = "icon";
+    
     public static ProgressBar progressBar;
+    
     public static ArrayList<JsonData> data;
+    
     private static ListView listView;
+    
     static JsonArrayAdapter adapter;
+    
+    
     private static Object object;
     /** Called when the activity is first created. */
     @Override
@@ -53,18 +70,25 @@ public class BuyersGuideActivity extends Activity {
 		inflater.inflate(R.menu.list_menu,menu);
 		return true;
 	}
-	
+    
+    
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
 		switch(item.getItemId()){
 		case R.id.Settings:
+			Intent intent = new Intent(BuyersGuideActivity.this, Preferences.class);
+			startActivity(intent);
+			Toast.makeText(BuyersGuideActivity.this,"Hello",Toast.LENGTH_LONG).show();
 			return true;
-	    default:
-	    	return super.onOptionsItemSelected(item);
+		case R.id.comments:
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
 	}
-    
-    public static void fillData(){
+	
+	
+	public static void fillData(){
     	BuyersGuideActivity.progressBar.setVisibility(ProgressBar.GONE);
     	BuyersGuideActivity.adapter = new JsonArrayAdapter((Context)object,R.layout.list_item,data);
     	BuyersGuideActivity.listView.setAdapter(BuyersGuideActivity.adapter);
